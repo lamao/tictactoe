9e472bd94a0caf0bf7b3908b6deb0f98178ed583
vmischeryakov <vmischeryakov@gmail.com>
2017-07-29 20:24:06 +0300

    Add initial data. Markup changes.

--- src/main/resources/sql/init-data.sql
+++ src/main/resources/sql/init-data.sql
@@ -6,16 +6,19 @@ insert into state (st_id, st_code, st_title) values
 
 insert into location (lc_id, lc_x, lc_y) values
  (1, 0, 1),
- (2, 1, 1);
+ (2, 1, 1),
+ (3, 1, 2);
 
 insert into snapshot (sn_id, sn_last_turn_id, sn_dump) values
-(1, null,  '         '),
-(2, 1,     ' o x o x '),
-(3, 2,     'o   x    ');
+(1, null, '         '),
+(2, 1,    ' o x o x '),
+(3, 2,    'o   x    '),
+(4, 3,    'ox ox  x ');
 
 
 
 insert into game (gm_id, gm_state_id, gm_title, gm_snapshot_id) values
-(1, 1, 'First game',     1),
-(2, 1, 'The second one', 2),
-(3, 1, 'The third',      3);
+(1, 1, 'Empty game',      1),
+(2, 1, 'The second one',  2),
+(3, 1, 'The third',       3),
+(4, 2, 'Winner game',     4);
